fc2a73f33a2520bb52acb974c4a7e5202b84c2b6
Rocklin K S <rocklin2603@gmail.com>
2025-04-17 06:25:24 +0000

    Update rofi-powermenu.sh

--- config/polybar/scripts/rofi-powermenu.sh
+++ config/polybar/scripts/rofi-powermenu.sh
@@ -40,22 +40,44 @@ run_rofi() {
 	echo -e "$option_1\n$option_2\n$option_3\n$option_5\n$option_6" | rofi_cmd
 }
 
+# Detect session (lowercase for easier matching)
+session="${XDG_CURRENT_DESKTOP,,}"
 
 chosen="$(run_rofi)"
-case ${chosen} in
-    $option_1)
-		betterlockscreen -l
+case "$chosen" in
+    "$option_1")
+        if [[ "$session" == *xfce* ]]; then
+            xflock4
+        elif [[ "$session" == *cinnamon* ]]; then
+            cinnamon-screensaver-command --lock
+        elif [[ "$session" == *plasma* ]]; then
+            # KDE Plasma 6
+            qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock
+        elif [[ "$session" == *gnome* ]]; then
+            # GNOME
+            gnome-screensaver-command -l 2>/dev/null || loginctl lock-session
+        fi
         ;;
-    $option_2)
-		qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
+    "$option_2")
+        if [[ "$session" == *xfce* ]]; then
+            xfce4-session-logout --logout
+        elif [[ "$session" == *cinnamon* ]]; then
+            cinnamon-session-quit --logout --no-prompt
+        elif [[ "$session" == *plasma* ]]; then
+            # KDE Plasma 6
+            qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
+        elif [[ "$session" == *gnome* ]]; then
+            # GNOME
+            gnome-session-quit --logout --no-prompt
+        fi
         ;;
-    $option_3)
-		systemctl suspend
+    "$option_3")
+        systemctl suspend
         ;;
-    $option_5)
-		systemctl reboot
+    "$option_5")
+        systemctl reboot
         ;;
-    $option_6)
-		systemctl poweroff
+    "$option_6")
+        systemctl poweroff
         ;;
 esac
